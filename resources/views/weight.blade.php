b45434e7d2c547ecfecf6d52419f01ba6f52fd47
Pringal <pringal@Pringals-MacBook-Pro-M1.local>
2022-11-25 21:59:35 +0530

    added tailwind css

--- resources/views/weight.blade.php
+++ resources/views/weight.blade.php
@@ -22,6 +22,7 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
+        @vite('resources/css/app.css')
     </head>
     <body class="antialiased">
     <div id="app">
